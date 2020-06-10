4001765e51b73fdbe9f933e3f3401fa26ca01713
NAOTO03 <1816011@s.asojuku.ac.jp>
2020-06-10 19:40:49 +0900

    key用の関数追加

--- main.cpp
+++ main.cpp
@@ -10,6 +10,30 @@
 
 int gameCount;
 
+char key[256];
+
+// キーの入力状態を更新する
+int UpdateKey() 
+{
+	char tmpKey[256]; // 現在のキーの入力状態を格納する
+	GetHitKeyStateAll(tmpKey); // 全てのキーの入力状態を得る
+
+	for (int i = 0; i < 256; i++)
+	{
+		if (tmpKey[i] != 0)
+		{ 
+			// i番のキーコードに対応するキーが押されていたら
+			key[i]++;     // 加算
+		}
+		else
+		{              
+			// 押されていなければ
+			key[i] = 0;   // 0にする
+		}
+	}
+	return 0;
+}
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	ChangeWindowMode(TRUE);
@@ -38,6 +62,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 		// 画面を初期化(真っ黒にする)
 		ClearDrawScreen();
+
+		UpdateKey();
 
 		sceneMgr.All();
 
