b930b29f89ca32c8a34d2f3dde79e8477e7dbd1d
NAOTO03 <1816011@s.asojuku.ac.jp>
2020-09-03 13:11:12 +0900

    #define から constexpr autoに 変更

--- define.h
+++ define.h
@@ -8,58 +8,58 @@ extern int gameCount;
 
 // ---------- 当たり判定用半径定義 ----------
 
-#define PLAYER_COLLISION 4	// プレイヤーの当たり判定用
-#define ENEMY_COLLISION 12  // エネミーの当たり判定用
+constexpr auto PLAYER_COLLISION = 4;  // プレイヤーの当たり判定用
+constexpr auto ENEMY_COLLISION = 12;  // エネミーの当たり判定用
 
-#define PSHOT_COLLISION 4	// プレイヤーの弾
-#define ESHOT0_COLLISION 6  // エネミーの弾(丸い弾)
-#define ESHOT1_COLLISION 4	// 縦長の弾
-#define ESHOT2_COLLISION 4  // ひし形の小さな弾
+constexpr auto PSHOT_COLLISION = 4;	  // プレイヤーの弾
+constexpr auto ESHOT0_COLLISION = 6;  // エネミーの弾(丸い弾)
+constexpr auto ESHOT1_COLLISION = 4;  // 縦長の弾
+constexpr auto ESHOT2_COLLISION = 4;  // ひし形の小さな弾
 
-#define BOSS_COLLISION 60   // ボスの当たり判定用
+constexpr auto BOSS_COLLISION = 60;   // ボスの当たり判定用
 
-#define ITEM_COLLISION 10   // アイテムの当たり判定用半径
+constexpr auto ITEM_COLLISION = 10;   // アイテムの当たり判定用半径
 
 
 // ---------- プレイヤー関連 ----------
 
 //プレイヤーの初期位置
-#define PLAYER_INITX 310
-#define PLAYER_INITY 550
+constexpr auto PLAYER_INITX = 310;
+constexpr auto PLAYER_INITY = 550;
 //プレイヤーの移動スピード
-#define PLAYER_SPEED 3
+constexpr auto PLAYER_SPEED = 3;
 //プレイヤーの弾の数
-#define PSHOT_NUM 200
+constexpr auto PSHOT_NUM = 200;
 //弾スピード
-#define PSHOT_SPEED 8
+constexpr auto PSHOT_SPEED = 8;
 // プレイヤーのエフェクトの数
-#define EFFECT_PDEADNUM 80
+constexpr auto EFFECT_PDEADNUM = 80;
 
 // ---------- エネミー関連 ----------
 
 // エネミーの数
-#define ENEMY_NUM 54
+constexpr auto ENEMY_NUM = 54;
 // エネミーの弾の数
-#define ESHOT_NUM 300
+constexpr auto ESHOT_NUM = 300;
 // エネミーのエフェクトの数
-#define EFFECT_EDEADNUM 50
+constexpr auto EFFECT_EDEADNUM = 50;
 
 // ---------- ボス関連 ----------
 
 // ボスの弾の数
-#define BOSS_SHOTNUM 300
+constexpr auto BOSS_SHOTNUM = 300;
 // ボスの揺れ幅
-#define BOSS_SHAKE 20
+constexpr auto BOSS_SHAKE = 20;
 // ボスのHP
-#define BOSS_HP 1500
+constexpr auto BOSS_HP = 1500;
 
 // ---------- 背景関連 ----------
 
 // スコアの文字描画の基準となるX座標
-#define SCORE_X 660
+constexpr auto SCORE_X = 660;
 
 //アイテムの総数
-#define ITEM_NUM 30
+constexpr auto ITEM_NUM = 30;
 
 
 
@@ -116,8 +116,8 @@ struct ESHOT
 
 // ---------- 背景関連 ----------
 
-struct FILE_DATA
-{
-	int score;
-	int highScore;
-};
+//struct FILE_DATA
+//{
+//	int score;
+//	int highScore;
+//};
